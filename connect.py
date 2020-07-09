49a8b83c7dedc86531ad3cfc47534cd9d7c39602
Duy Tran <duy2271995@gmail.com>
2020-07-09 20:07:26 +0700

    Update connect.py

--- connect.py
+++ connect.py
@@ -8,3 +8,13 @@ device = {
     "secret":"admin"
 }
 
+ssh = ConnectHandler(**device)
+ssh.enable()
+#ssh.send_config_set("hostname duy")
+#print(ssh.send_config_set("hostname duy"))
+command = ["int loop 0", "ip add 172.16.1.1 255.255.255.0"]
+ssh.send_config_set(command)
+print(ssh.send_config_set("do sh ip int br"))
+output = ssh.find_prompt()
+print(output)
+ssh.disconnect()
